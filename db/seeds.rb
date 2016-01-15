a10bb1843031c269bd663080a97f293242962ca7
beckylehmann91 <beckylehmann91@gmail.com>
2016-01-15 14:21:05 -0600

    Edit security information

--- db/seeds.rb
+++
@@ -1,5 +0,0 @@
-User.create(username: "becky", password: "becky", sid: "AC807bb5e2a8247b3d1dc56f18e6a998ce", auth_token: "3ed1a743347ada915f6ede1c4917f601", twilio_number: "+18722398382", raw_number: "+17814136983")
-
-User.create(username: "josh", password: "josh", sid: "ACaccfb5268cb23b1d2daa4f556e6ba64a", twilio_number: "+17735969397", raw_number: "+17733439318", auth_token: "08ae13cdabed63a3a86b2b8ad999465a")
-
-User.create(username: "will", password: "will", sid: "AC8ef4d575744d2d3cda6b90d125ffae70", twilio_number: "+2057917555", raw_number: "+12054013355", auth_token: "ae45c904bcf77b5cce6e614f5f16f0f7")
